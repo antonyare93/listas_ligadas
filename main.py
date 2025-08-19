18172e80a85504aee7a14c8caf29fc6aa0f6d76a
Antony Arévalo <antonyare_93@hotmail.com>
2025-08-19 23:53:58 +0000

    Types added and changes has been made

--- main.py
+++ main.py
@@ -1,66 +1,74 @@
 # %%
-class Nodo():
-    def __init__(self, valor1, valor2):
-        self.dato1 = valor1
-        self.dato2 = valor2
-        self.siguente = None
+from typing import Optional
 
 # %%
-class LSL:
+class Nodo():
+    def __init__(self, id, edad, salario):
+        self.id: str = id
+        self.edad: int = edad
+        self.salario: float = salario
+        self.li: Optional['Nodo'] = None
+
+# %%
+class LSL3:
     '''Clase que define una Lista Simplemente Ligada
     - Metodo que inicializa la lista
     - insertar: Metodo que inserta un nodo en la lista
-    - mostrar: Metodo que imprime el contenido de la lista'''
+    - imprimir: Metodo que imprime el contenido de la lista'''
 
     # Aca se inicializa el objeto LSL
     def __init__(self):
-        self.inicio = None
+        self.inicio: Optional[Nodo] = None
 
     # Aca se inicia el metodo con el que inserta
-    def insertar(self, valor1, valor2):
-        nuevo_nodo = Nodo(valor1, valor2)
+    def insertar(self, id: str, edad: int, salario: float):
+        nuevo_nodo = Nodo(id, edad, salario)
 
         if not self.inicio:
             self.inicio = nuevo_nodo
         else:
             nodo_actual = self.inicio
-            while nodo_actual.siguente:
-                nodo_actual = nodo_actual.siguente
-            nodo_actual.siguente = nuevo_nodo
+            while nodo_actual.li:
+                nodo_actual = nodo_actual.li
+            nodo_actual.li = nuevo_nodo
 
 
     # Aca se muestra el metodo que imprime el contenido de la lista
-    def mostrar(self):
+    def imprimir(self):
         nodo_actual = self.inicio
+        contador: int = 0
         while nodo_actual:
-            print(f'Dato1: {nodo_actual.dato1} // Dato2: {nodo_actual.dato2}')
-            nodo_actual = nodo_actual.siguente
+            print(f'Identificación: {nodo_actual.id} // Edad: {str(nodo_actual.edad)} // Salario: {str(nodo_actual.salario)}')
+            nodo_actual = nodo_actual.li
+            contador += 1
+        print(f'Total de nodos: {contador}')
 
     # Aca se va a eliminar un nodo
-    def eliminar(self, valor1, valor2):
+    def eliminar(self, id: str):
         nodo_actual = self.inicio
         if nodo_actual:
-            if nodo_actual.dato1 == valor1 and nodo_actual.dato2 == valor2:
-                self.inicio = nodo_actual.siguente
+            if nodo_actual.id == id:
+                self.inicio = nodo_actual.li
                 return
-            while nodo_actual.siguente:
-                if nodo_actual.siguente.dato1 == valor1 and nodo_actual.siguente.dato2 == valor2:
-                    nodo_actual.siguente = nodo_actual.siguente.siguente
+            while nodo_actual.li:
+                if nodo_actual.li.id == id:
+                    nodo_actual.li = nodo_actual.li.li
                     return
-                nodo_actual = nodo_actual.siguente
+                nodo_actual = nodo_actual.li
+        print('La lista está vacía, no se puede eliminar el nodo.')
 
 # %%
 def main():
-    lista = LSL()
-    lista.insertar(1,2)
-    lista.insertar(3,4)
-    lista.insertar(5,6)
-    lista.insertar(7,8)
-    lista.insertar(9,10)
-    lista.eliminar(1,2)
-    lista.eliminar(5,6)
-    lista.eliminar(9,10)
-    lista.mostrar()
+    lista = LSL3()
+    lista.insertar('1',2,3)
+    lista.insertar('4',5,6)
+    lista.insertar('7',8,9)
+    lista.insertar('10',11,12)
+    lista.insertar('13',14,15)
+    lista.eliminar('13')
+    lista.eliminar('1')
+    lista.eliminar('7')
+    lista.imprimir()
 # %%
 if __name__ == '__main__':
     main()
